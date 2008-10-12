3842f29b88412d8ec0f05f977e2fbeaaab5934c0
edwin <edwin@FreeBSD.org>
2008-10-12 10:10:42 +0000

    It all started when we got some new routers, which told me the following when trying to upload configuration or download images from it: The TFTP server doesn't support the blocksize option.
    My curiousity was triggered, it took me some reading of RFCs and
    other documentation to find out what was possible and what could
    be done. Was plain TFTP very simple in its handshake, TFTP with
    options was kind of messy because of its backwards capability: The
    first packet returned could either be an acknowledgement of options,
    or the first data packet.
    
    Going through the source code of src/libexec/tftpd and going through
    the code of src/usr.bin/tftp showed that there was a lot of duplicate
    code, and the addition of options would only increase the amount
    of duplicate code. After all, both the client and the server can
    act as a sender and receiver.
    
    At the end, it ended up with a nearly complete rewrite of the tftp
    client and server. It has been tested against the following TFTP
    clients and servers:
    
    - Itself (yay!)
    - The standard FreeBSD tftp client and server
    - The Fedora Core 6 tftp client and server
    - Cisco router tftp client
    - Extreme Networks tftp client
    
    It supports the following RFCs:
    
    RFC1350 - THE TFTP PROTOCOL (REVISION 2)
    RFC2347 - TFTP Option Extension
    RFC2348 - TFTP Blocksize Option
    RFC2349 - TFTP Timeout Interval and Transfer Size Options
    RFC3617 - Uniform Resource Identifier (URI) Scheme and Applicability
              Statement for the Trivial File Transfer Protocol (TFTP)
    
    It supports the following unofficial TFTP Options as described at
    http://www.compuphase.com/tftp.htm:
    
    blksize2 - Block size restricted to powers of 2, excluding protocol headers
    rollover - Block counter roll-over (roll back to zero or to one)
    
    From the tftp program point of view the following things are changed:
    
    - New commands: "blocksize", "blocksize2", "rollover" and "options"
    - Development features: "debug" and "packetdrop"
    
    If you try this tftp/tftpd implementation, please let me know if
    it works (or doesn't work) and against which implementaion so I can
    get a list of confirmed working systems.

--- libexec/tftpd/tftp-transfer.h
+++ libexec/tftpd/tftp-transfer.h
@@ -1,7 +1,6 @@
 /*
- * Copyright (c) 1993
- *	The Regents of the University of California.  All rights reserved.
- *
+ * Copyright (C) 2008 Edwin Groothuis. All rights reserved.
+ * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
@@ -10,18 +9,11 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * 
+ * THIS SOFTWARE IS PROVIDED BY AUTHOR AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE
  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
@@ -29,10 +21,11 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- *
- *	@(#)extern.h	8.1 (Berkeley) 6/6/93
- * $FreeBSD$
  */
 
-void	recvfile(int, char *, char *);
-void	xmitfile(int, char *, char *);
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
+
+void	tftp_send(int peer, uint16_t *block, struct tftp_stats *tp);
+void	tftp_receive(int peer, uint16_t *block, struct tftp_stats *tp,
+	    struct tftphdr *firstblock, size_t fb_size);
