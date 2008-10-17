de21433e8c854844df013076cdd5e35677ad4f9f
edwin <edwin@FreeBSD.org>
2008-10-17 23:26:44 +0000

    This is an experiment.
    Contributed software, like the timezone database, is always imported,
    often a merge-from-vendor is done and most of the times it is MFCd
    into the latest branch and the branch before that.
    
    Updating the release note documentation is a thing which is sometimes
    done, sometimes done by a third person and sometimes not done at
    all.
    
    By creating a datafile to store the information about the software,
    its versions, its import dates, its MFV dates and its MFC dates, I
    hope that it will be easier for commiters (lazy bums as they are!)
    and re@ to make sure that the information about these updates is
    tracked and easy to be used to generate release notes.
    
    First steps:
    Create a copy of the release directories for the 6.x and higher
    releases and branches, adjust the article.sgml files 7.x and higher
    releases and branches.
    Create the right catalog for the contributed software entity.
    Create the configuration file and the beginning of a script to
    convert the configuration into a piece of information which can be
    used by the releasenote processing software.

---
+++ contrib-xmltoent.pl
@@ -0,0 +1,176 @@
+#!/usr/bin/perl -w
+
+use XML::Parser;
+use Data::Dumper;
+use strict;
+
+my @tree = ();
+my @values = ();
+my $treeindex = -1;
+
+my %branches = ();
+my %softwares = ();
+my $software = "";
+my $mfcbranch = "";
+my $swversion = "";
+
+my %releaseent = ();
+
+sub xml_start {
+	my $expat = shift;
+	my $element = shift;
+
+	$tree[++$treeindex] = $element;
+	while (defined (my $attribute = shift)) {
+		$values[$treeindex]{$attribute} = shift;
+	}
+
+	if ($element eq "software") {
+		$software = $values[$treeindex]{name};
+	}
+	if ($element eq "version") {
+		$softwares{$software}{versions}{$values[$treeindex]{version}} = {};
+		$swversion = $values[$treeindex]{version};
+	}
+	if ($element eq "mfc") {
+		$mfcbranch = $values[$treeindex]{branch};
+	}
+
+}
+
+sub xml_end {
+	my $expat = shift;
+	my $element = shift;
+
+	$values[$treeindex] = ();
+	$treeindex--;
+}
+
+sub xml_char {
+	my $expat = shift;
+	my $value = shift;
+
+	if ($tree[0] eq "freebsd") {
+		return if ($treeindex == 0);
+
+		if ($tree[1] eq "branches") {
+			return if ($treeindex == 1);
+
+			if ($tree[2] eq "branch") {
+				$branches{$values[$treeindex]{name}} = $value;
+				return;
+			}
+
+			return;
+		}
+
+		if ($tree[1] eq "softwares") {
+			return if ($treeindex == 1);
+
+			if ($tree[2] eq "software") {
+				return if ($treeindex == 2);
+
+				if ($tree[3] eq "desc") {
+					$softwares{$software}{desc} = ""
+						if (!defined $softwares{$software}{desc});
+					$softwares{$software}{desc} .= $value;
+					return;
+				}
+
+				if ($tree[3] eq "versions") {
+					return if ($treeindex == 3);
+
+					if ($tree[4] eq "version") {
+						return if ($treeindex == 4);
+
+						if ($tree[5] eq "import") {
+							$softwares{$software}{versions}{$swversion}{import} = $value;
+							return;
+						}
+						if ($tree[5] eq "mfv") {
+							$softwares{$software}{versions}{$swversion}{mfv} = $value;
+							return;
+						}
+						if ($tree[5] eq "desc") {
+							$softwares{$software}{versions}{$swversion}{desc} = ""
+								if (!defined $softwares{$software}{versions}{$swversion}{desc});
+							$softwares{$software}{versions}{$swversion}{desc} .= $value;
+							return;
+						}
+						if ($tree[5] eq "mfc") {
+							$softwares{$software}{versions}{$swversion}{mfc}{$mfcbranch} = $value;
+							return;
+						}
+
+					}
+				}
+			}
+		}
+	}
+
+}
+
+my $p = new XML::Parser(
+	Handlers => {
+		Start   => \&xml_start,
+		End     => \&xml_end,
+		Char    => \&xml_char,
+	});
+$p->parsefile("../../../../../contrib.xml");
+
+{
+	my %r = (
+		"release.current"	=> 1,
+		"release.next"		=> 1,
+		"release.prev"		=> 1,
+		"release.branch"	=> 1,
+	);
+	open(FIN, "../../share/sgml/release.ent");
+	my @lines = <FIN>;
+	close(FIN);
+	chomp(@lines);
+
+	foreach my $line (@lines) {
+		if ($line =~ /<!ENTITY ([^ ]+) "([^\-]+).*">/) {
+			next if (!defined $r{$1});
+			$releaseent{$1} = $2;
+		}
+	}
+}
+
+#print Dumper(%branches);
+#print Dumper(%releaseent);
+#print Dumper(%softwares);
+
+#
+# If we are in -current, then release.current doesn't exist yet.
+# In that case copy all MFVs into MFC{release.branch}. The date
+# of the new MFC is the date of the release.prev.
+#
+# release.current is then assigned with release.branch.
+#
+# the creation date of branches{release.current} will be
+# branches{release.prev}
+#
+if (!defined $branches{$releaseent{"release.current"}}) {
+	foreach my $sw (keys(%softwares)) {
+		foreach my $vs (keys(%{$softwares{$sw}{versions}})) {
+			next if (!defined $softwares{$sw}{versions}{$vs}{mfv});
+			$softwares{$sw}{versions}{$vs}{mfc}{$releaseent{"release.branch"}} = $softwares{$sw}{versions}{$vs}{mfv};
+		}
+	}
+
+	$releaseent{"release.current"} = $releaseent{"release.branch"};
+	$branches{$releaseent{"release.current"}} =
+		$branches{$releaseent{"release.prev"}}
+
+}
+
+#
+# Find all MFVs which are done in the time that 
+#
+
+
+print Dumper(%branches);
+print Dumper(%releaseent);
+#print Dumper(%softwares);
