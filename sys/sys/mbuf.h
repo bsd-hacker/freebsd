298546a0e1e47c5712c78a054316b80ab54f47d6
kmacy <kmacy@FreeBSD.org>
2008-11-04 06:24:46 +0000

    fix ordering to avoid padding on 64-bit and add rss_hash field

--- sys/sys/mbuf.h
+++ sys/sys/mbuf.h
@@ -121,6 +121,7 @@ struct pkthdr {
 	u_int16_t	 tso_segsz;	/* TSO segment size */
 	u_int16_t	 ether_vtag;	/* Ethernet 802.1p+q vlan tag */
 	SLIST_HEAD(packet_tags, m_tag) tags; /* list of packet tags */
+        uint32_t	rss_hash;	/* hash identifying the flow */
 };
 
 /*
@@ -133,8 +134,8 @@ struct m_ext {
 			    (void *, void *);
 	void		*ext_arg1;	/* optional argument pointer */
 	void		*ext_arg2;	/* optional argument pointer */
-	u_int		 ext_size;	/* size of buffer, for ext_free */
 	volatile u_int	*ref_cnt;	/* pointer to ref count info */
+	u_int		 ext_size;	/* size of buffer, for ext_free */
 	int		 ext_type;	/* type of external storage */
 };
 
