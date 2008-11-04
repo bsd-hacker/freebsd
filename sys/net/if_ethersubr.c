9171d1acae8b559a28d983b035d961ce71874f72
kmacy <kmacy@FreeBSD.org>
2008-11-04 06:30:20 +0000

    replace IFQ_HANDOFF in ether_output_frame with call to ifp's if_start_mbuf

--- sys/net/if_ethersubr.c
+++ sys/net/if_ethersubr.c
@@ -408,7 +408,6 @@ bad:			if (m != NULL)
 int
 ether_output_frame(struct ifnet *ifp, struct mbuf *m)
 {
-	int error;
 #if defined(INET) || defined(INET6)
 	INIT_VNET_NET(ifp->if_vnet);
 	struct ip_fw *rule = ip_dn_claim_rule(m);
@@ -428,8 +427,7 @@ ether_output_frame(struct ifnet *ifp, struct mbuf *m)
 	 * Queue message on interface, update output statistics if
 	 * successful, and start output if interface not yet active.
 	 */
-	IFQ_HANDOFF(ifp, m, error);
-	return (error);
+	return ((ifp->if_start_mbuf)(ifp, m));
 }
 
 #if defined(INET) || defined(INET6)
