fea3a987b5cde5a87888f4a0bfb4cee0c39e24dc
kmacy <kmacy@FreeBSD.org>
2008-12-29 22:07:18 +0000

    cache rtentry in inpcb for connected sockets

--- sys/netinet/tcp_usrreq.c
+++ sys/netinet/tcp_usrreq.c
@@ -1077,6 +1077,7 @@ tcp_connect(struct tcpcb *tp, struct sockaddr *nam, struct thread *td)
 	struct socket *so = inp->inp_socket;
 	INIT_VNET_INET(so->so_vnet);
 	struct in_addr laddr;
+	struct route sro;
 	u_short lport;
 	int error;
 
@@ -1105,6 +1106,7 @@ tcp_connect(struct tcpcb *tp, struct sockaddr *nam, struct thread *td)
 	inp->inp_laddr = laddr;
 	in_pcbrehash(inp);
 
+	in_pcbrtalloc(inp, inp->inp_faddr.s_addr, &sro);
 	/*
 	 * Compute window scaling to request:
 	 * Scale to fit into sweet spot.  See tcp_syncache.c.
