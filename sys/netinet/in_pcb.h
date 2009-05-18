9acb10dcc779f963a1c028c29670982695ba9ae2
kmacy <kmacy@FreeBSD.org>
2009-05-18 17:57:33 +0000

    glue in flowid

--- sys/netinet/in_pcb.h
+++ sys/netinet/in_pcb.h
@@ -168,7 +168,7 @@ struct inpcb {
 	u_char	inp_ip_ttl;		/* (i) time to live proto */
 	u_char	inp_ip_p;		/* (c) protocol proto */
 	u_char	inp_ip_minttl;		/* (i) minimum TTL or drop */
-	uint32_t inp_ispare1;		/* (x) connection id / queue id */
+	uint32_t inp_flowid;		/* (x) connection id / queue id */
 	u_int	inp_refcount;		/* (i) refcount */
 	void	*inp_pspare[2];		/* (x) rtentry / general use */
 
