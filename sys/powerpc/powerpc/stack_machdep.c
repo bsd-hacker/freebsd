1313ac36b9c206f08254f64eb9b874b1a2478865
nwhitehorn <nwhitehorn@FreeBSD.org>
2009-09-07 15:43:09 +0000

    Fix stack_machdep.c to work correctly by taking into account the TOC reload slot offset when determining if we are coming out of a trap. This avoids a nested fault when init exits.

--- sys/powerpc/powerpc/stack_machdep.c
+++ sys/powerpc/powerpc/stack_machdep.c
@@ -42,6 +42,12 @@ __FBSDID("$FreeBSD$");
 #include <machine/stack.h>
 #include <machine/trap.h>
 
+#ifdef __powerpc64__
+#define CALLOFFSET 8 /* Account for the TOC reload slot */
+#else
+#define CALLOFFSET 4
+#endif
+
 static void
 stack_capture(struct stack *st, register_t frame)
 {
@@ -54,7 +60,12 @@ stack_capture(struct stack *st, register_t frame)
 		frame = *(register_t *)frame;
 		if (frame < PAGE_SIZE)
 			break;
+
+	    #ifdef __powerpc64__
+		callpc = *(vm_offset_t *)(frame + 16) - 4;
+	    #else
 		callpc = *(vm_offset_t *)(frame + 4) - 4;
+	    #endif
 		if ((callpc & 3) || (callpc < 0x100))
 			break;
 
@@ -64,8 +75,8 @@ stack_capture(struct stack *st, register_t frame)
 		 * things are going wrong. Plus, prevents this shortened
 		 * version of code from accessing user-space frames
 		 */
-		if (callpc + 4 == (register_t) &trapexit ||
-		    callpc + 4 == (register_t) &asttrapexit)
+		if (callpc + CALLOFFSET == (register_t) &trapexit ||
+		    callpc + CALLOFFSET == (register_t) &asttrapexit)
 			break;
 
 		if (stack_put(st, callpc) == -1)
