07839eaae3cb9c5177c94db539e45a6b2674700c
edwin <edwin@FreeBSD.org>
2009-12-31 07:28:13 +0000

    As requested by Robert Watson:
    - Properly assign copyright for locale.c, since it came from day.c
    - Non-pseuonymous dates.c and parsedata.c

--- parsedata.c
+++ parsedata.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 1992-2009 The FreeBSD Project. All rights reserved.
+ * Copyright (c) 1992-2009 Edwin Groothuis. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
