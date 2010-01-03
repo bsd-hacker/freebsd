8029c0d2d937be1429c87578bceee24de0f7580f
kmacy <kmacy@FreeBSD.org>
2010-01-03 10:52:24 +0000

    further grinding away of missing symbols

---
+++ lib/libunet/unet_compat.c
@@ -0,0 +1,44 @@
+#undef _KERNEL
+#define _WANT_UCRED
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/refcount.h>
+#include <sys/ucred.h>
+
+struct malloc_type;
+
+void *
+unet_malloc(unsigned long size, struct malloc_type *type, int flags)
+{
+
+	return (malloc(size));
+}
+
+void
+unet_free(void *addr, struct malloc_type *type)
+{
+
+	free(addr);
+}
+
+/*
+ * Claim another reference to a ucred structure.
+ */
+struct ucred *
+crhold(struct ucred *cr)
+{
+
+	refcount_acquire(&cr->cr_ref);
+	return (cr);
+}
+
+/*
+ * Free a cred structure.  Throws away space when ref count gets to 0.
+ */
+void
+crfree(struct ucred *cr)
+{
+	if (refcount_release(&cr->cr_ref)) {
+		free(cr);
+	}
+}       
---
+++ lib/libunet/unet_glue.c
@@ -0,0 +1,86 @@
+#include <sys/cdefs.h>
+
+#include <sys/param.h>
+#include <sys/types.h>
+#include <sys/kernel.h>
+#include <sys/refcount.h>
+#include <sys/systm.h>
+#include <sys/jail.h>
+#include <sys/priv.h>
+
+int
+prison_if(struct ucred *cred, struct sockaddr *sa)
+{
+
+	return (0);
+}
+
+int
+prison_check_af(struct ucred *cred, int af)
+{
+
+	return (0);
+}
+
+int
+prison_check_ip4(struct ucred *cred, struct in_addr *ia)
+{
+
+	return (0);
+}
+
+
+int
+prison_equal_ip4(struct prison *pr1, struct prison *pr2)
+{
+
+	return (1);
+}
+
+/*
+ * See if a prison has the specific flag set.
+ */
+int
+prison_flag(struct ucred *cred, unsigned flag)
+{
+
+	/* This is an atomic read, so no locking is necessary. */
+	return (flag & PR_HOST);
+}
+
+int
+prison_get_ip4(struct ucred *cred, struct in_addr *ia)
+{
+
+	return (0);
+}
+
+int
+prison_local_ip4(struct ucred *cred, struct in_addr *ia)
+{
+
+	return (0);
+}
+
+int
+prison_remote_ip4(struct ucred *cred, struct in_addr *ia)
+{
+
+	return (0);
+}
+
+int
+priv_check(struct thread *td, int priv)
+{
+
+	return (0);
+}
+
+int
+priv_check_cred(struct ucred *cred, int priv, int flags)
+{
+
+	return (0);
+}
+
+
