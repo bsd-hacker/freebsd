17686f0fca27be0361e49fed81390f157811ab32
kmacy <kmacy@FreeBSD.org>
2010-01-18 03:58:23 +0000

    ifdef out kproc_kthread_add

--- lib/libunet/unet_sys/unet_kern_intr.c
+++ lib/libunet/unet_sys/unet_kern_intr.c
@@ -361,6 +361,7 @@ ithread_create(const char *name)
 
 	ithd = malloc(sizeof(struct intr_thread), M_ITHREAD, M_WAITOK | M_ZERO);
 
+#ifdef notyet
 	error = kproc_kthread_add(ithread_loop, ithd, &intrproc,
 		    &td, RFSTOPPED | RFHIGHPID,
 	    	    0, "intr", "%s", name);
@@ -368,6 +369,7 @@ ithread_create(const char *name)
 		panic("kproc_create() failed with %d", error);
 	td->td_pflags |= TDP_ITHREAD;
 	ithd->it_thread = td;
+#endif	
 	CTR2(KTR_INTR, "%s: created %s", __func__, name);
 	return (ithd);
 }
