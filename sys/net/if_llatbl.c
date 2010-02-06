a313700161992eb517c8027857716e589384912a
kmacy <kmacy@FreeBSD.org>
2010-02-06 22:50:50 +0000

    clarify calling convention for llentry_update slightly

--- sys/net/if_llatbl.c
+++ sys/net/if_llatbl.c
@@ -111,7 +111,7 @@ llentry_free(struct llentry *lle)
 
 /*
  * Update an llentry for address dst (equivalent to rtalloc for new-arp)
- * Caller must pass in a valid struct llentry *
+ * Caller must pass in a valid struct llentry * (or NULL)
  *
  * if found the llentry * is returned referenced and unlocked
  */
