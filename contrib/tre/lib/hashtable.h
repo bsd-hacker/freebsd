3fc69be8c42796e92f41187effba3e1b44f80362
gabor <gabor@FreeBSD.org>
2011-08-25 01:47:26 +0000

    - Convert to TRE style

--- contrib/tre/lib/hashtable.h
+++ contrib/tre/lib/hashtable.h
@@ -5,29 +5,31 @@
 
 #include <sys/types.h>
 
-#define HASH_OK		0
-#define HASH_UPDATED	1
-#define HASH_FAIL	2
-#define HASH_FULL	3
-#define HASH_NOTFOUND	4
+#define HASH_OK         0
+#define HASH_UPDATED    1
+#define HASH_FAIL       2
+#define HASH_FULL       3
+#define HASH_NOTFOUND   4
+
+#define HASHSTEP(x,c) (((x << 5) + x) + (c))
 
 typedef struct {
-	void		*key;
-	void		*value;
+  void *key;
+  void *value;
 } hashtable_entry;
 
 typedef struct {
-	size_t		 key_size;
-	size_t		 table_size;
-	size_t		 usage;
-	size_t		 value_size;
-	hashtable_entry **entries;
+  size_t key_size;
+  size_t table_size;
+  size_t usage;
+  size_t value_size;
+  hashtable_entry **entries;
 } hashtable;
 
-void		 hashtable_free(hashtable *);
-int		 hashtable_get(hashtable *, const void *, void *);
-hashtable	*hashtable_init(size_t, size_t, size_t);
-int		 hashtable_put(hashtable *, const void *, const void *);
-int		 hashtable_remove(hashtable *, const void *);
+void hashtable_free(hashtable *);
+int hashtable_get(hashtable *, const void *, void *);
+hashtable *hashtable_init(size_t, size_t, size_t);
+int hashtable_put(hashtable *, const void *, const void *);
+int hashtable_remove(hashtable *, const void *);
 
 #endif	/* HASHTABLE.H */
