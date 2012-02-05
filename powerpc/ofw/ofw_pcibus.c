6660ddb590e5bd586b46d3bcc7b9a59b93c511c4
nwhitehorn <nwhitehorn@FreeBSD.org>
2012-02-05 00:31:07 +0000

    Choose to use a OF_xref_phandle() differently: for interrupt parents, use whatever is in the firmware tree for cross-referencing, and store the PIC id as the xref phandle (as we already do in xics).

--- powerpc/ofw/ofw_pcibus.c
+++ powerpc/ofw/ofw_pcibus.c
@@ -213,11 +213,10 @@ ofw_pcibus_enum_devtree(device_t dev, u_int domain, u_int busno)
 				icells = 1;
 				OF_getprop(child, "interrupt-parent", &iparent,
 				    sizeof(iparent));
-				iparent = OF_xref_phandle(iparent);
-
 				if (iparent != 0) {
-					OF_getprop(iparent, "#interrupt-cells",
-					    &icells, sizeof(icells));
+					OF_getprop(OF_xref_phandle(iparent),
+					    "#interrupt-cells", &icells,
+					    sizeof(icells));
 					intr[0] = MAP_IRQ(iparent, intr[0]);
 				}
 
@@ -346,8 +345,6 @@ ofw_pcibus_assign_interrupt(device_t dev, device_t child)
 	iparent = -1;
 	if (OF_getprop(node, "interrupt-parent", &iparent, sizeof(iparent)) < 0)
 		iparent = -1;
-	else
-		iparent = OF_xref_phandle(iparent);
 	
 	/*
 	 * Any AAPL,interrupts property gets priority and is
