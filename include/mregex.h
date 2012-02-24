9b82c84bfb48cc312caf473197db47c185b7d284
gabor <gabor@FreeBSD.org>
2012-02-24 13:13:55 +0000

    - Include missing header - Properly handle function names based on whether we are building libc   or not

--- include/mregex.h
+++ include/mregex.h
@@ -3,7 +3,36 @@
 
 #include <sys/types.h>
 
+#include <wchar.h>
 #include <regex.h>
+
+#ifndef TRE_LIBC_BUILD
+#define tre_mregncomp	mregncomp
+#define tre_mregcomp	mregcomp
+#define tre_mregnexec	mregnexec
+#define tre_mregexec	mregexec
+#define tre_mregfree	mregfree
+#define tre_mregwncomp	mregwncomp
+#define tre_mregwcomp	mregwcomp
+#define tre_mregwnexec	mregwnexec
+#define tre_mregwexec	mregwexec
+
+#define FUNC_DECL(f)    f
+
+#else
+
+#define mregncomp	tre_mregncomp
+#define mregcomp	tre_mregcomp
+#define mregnexec	tre_mregnexec
+#define mregexec	tre_mregexec
+#define mregfree	tre_mregfree
+#define mregwncomp	tre_mregwncomp
+#define mregwcomp	tre_mregwcomp
+#define mregwnexec	tre_mregwnexec
+#define mregwexec	tre_mregwexec
+
+#define FUNC_DECL(f)    tre_##f
+#endif
 
 typedef struct {
 	size_t k;		/* Number of patterns */
@@ -14,30 +43,31 @@ typedef struct {
 } mregex_t;
 
 int
-tre_mregncomp(mregex_t *preg, size_t nr, const char **regex,
+FUNC_DECL(mregncomp)(mregex_t *preg, size_t nr, const char **regex,
 	      size_t *n, int cflags);
 int
-tre_mregcomp(mregex_t *preg, size_t nr, const char **regex, int cflags);
+FUNC_DECL(mregcomp)(mregex_t *preg, size_t nr, const char **regex, int cflags);
+
 int
-tre_mregnexec(const mregex_t *preg, const char *str, size_t len,
+FUNC_DECL(mregnexec)(const mregex_t *preg, const char *str, size_t len,
 	      size_t nmatch, regmatch_t pmatch[], int eflags);
 int
-tre_mregexec(const mregex_t *preg, const char *str,
+FUNC_DECL(mregexec)(const mregex_t *preg, const char *str,
 	    size_t nmatch, regmatch_t pmatch[], int eflags);
 void
-tre_mregfree(mregex_t *preg);
+FUNC_DECL(mregfree)(mregex_t *preg);
 #ifdef TRE_WCHAR
 int
-tre_mregwncomp(mregex_t *preg, size_t nr, const wchar_t **regex,
+FUNC_DECL(mregwncomp)(mregex_t *preg, size_t nr, const wchar_t **regex,
 	       size_t *n, int cflags);
 int
-tre_mregwcomp(mregex_t *preg, size_t nr, const wchar_t **regex,
+FUNC_DECL(mregwcomp)(mregex_t *preg, size_t nr, const wchar_t **regex,
 	      int cflags);
 int
-tre_mregwnexec(const mregex_t *preg, const wchar_t *str, size_t len,
+FUNC_DECL(mregwnexec)(const mregex_t *preg, const wchar_t *str, size_t len,
 	       size_t nmatch, regmatch_t pmatch[], int eflags);
 int
-tre_mregwexec(const mregex_t *preg, const wchar_t *str,
+FUNC_DECL(mregwexec)(const mregex_t *preg, const wchar_t *str,
 	      size_t nmatch, regmatch_t pmatch[], int eflags);
 #endif /* TRE_WCHAR */
 
