3f65897d6bc290c9bb5dd2d5615c26f9e8fec53b
andre <andre@FreeBSD.org>
2012-10-21 11:16:16 +0000

    Simplify cache line alignment of global mutexes and remove the unnecessary MTX_GLB() accessor macro again.
    Suggested by:	glebius

--- sys/sys/mutex.h
+++ sys/sys/mutex.h
@@ -414,30 +414,15 @@ struct mtx_args {
 /*
  * Helper macros to prevent global mutexes to share a cache line
  * on SMP systems.
- * MTX_GLB(name) abstracts the access to the structure encapsulated
- * mutexes.
  */
 #ifdef SMP
 #define	MTX_GLOBAL(name)						\
-	struct {							\
-        	struct mtx (name);					\
-	} __aligned(CACHE_LINE_SIZE) (name)
+	struct mtx __aligned(CACHE_LINE_SIZE) (name)
 
-#define	MTX_GLOBAL_STATIC(name)						\
-	static struct {							\
-		struct mtx (name);					\
-	} __aligned(CACHE_LINE_SIZE) (name)
 #else /* SMP */
 #define	MTX_GLOBAL(name)						\
-	struct {							\
-		struct mtx (name);					\
-	} (name)
-#define MTX_GLOBAL_STATIC(name)						\
-	static struct {							\
-		struct mtx (name);					\
-	} (name)
+	struct mtx (name)
 #endif /* SMP */
-#define	MTX_GLB(name)	(&(name.name))
 
 /*
  * The INVARIANTS-enabled mtx_assert() functionality.
