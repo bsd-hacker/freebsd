7c8e420e3bfe7707ae1b074ac342a981da107a50
pho <pho@FreeBSD.org>
2012-11-16 07:30:38 +0000

    Added two GEOM test scenarios.

---
+++ misc/graid3.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+#
+# Copyright (c) 2012 Peter Holm <pho@FreeBSD.org>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+# $FreeBSD$
+#
+
+# Probably unrelated mkfifo() problem seen:
+# http://people.freebsd.org/~pho/stress/log/graid3.txt
+
+[ `id -u ` -ne 0 ] && echo "Must be root!" && exit 1
+
+. ../default.cfg
+
+md1=$mdstart
+md2=$((mdstart + 1))
+md3=$((mdstart + 2))
+
+for u in $md1 $md2 $md3; do
+	mdconfig -l | grep -q md$u && mdconfig -d -u $u
+	mdconfig -a -t swap -s 1g -u $u
+done
+
+graid3 load || exit
+graid3 label -v -r data md$md1 md$md2 md$md3 > /dev/null || exit
+newfs -U /dev/raid3/data  > /dev/null
+mount /dev/raid3/data $mntpoint
+chmod 777 $mntpoint
+
+export runRUNTIME=20m
+export RUNDIR=$mntpoint/stressX
+
+su $testuser -c 'cd ..; ./run.sh marcus.cfg'
+
+while mount | grep $mntpoint | grep -q raid3; do
+	umount $mntpoint || sleep 1
+done
+
+graid3 stop data
+graid3 unload
+
+for u in $md3 $md2 $md1; do
+	mdconfig -d -u $u
+done
