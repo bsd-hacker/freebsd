e4c749bef88cebaaace5e54b9a72e38b600fc7d3
crees <crees@FreeBSD.org>
2013-01-02 16:47:23 +0000

    Copyrights can't be owned by The FreeBSD Project apparently
    Submitted by:	theraven

--- rclint.py
+++ rclint.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright (c) 2012 The FreeBSD Project. All rights reserved.
+# Copyright (c) 2012 Chris Rees. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
