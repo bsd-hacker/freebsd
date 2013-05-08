ec03c0192732945b5de101c07e8e8f5787121297
pho <pho@FreeBSD.org>
2013-05-08 06:59:07 +0000

    Added shm_open(2) test scenario.

---
+++ misc/shm_open.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+
+#
+# Copyright (c) 2013 Peter Holm <pho@FreeBSD.org>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+# $FreeBSD$
+#
+
+# "panic: kmem_malloc(4096): kmem_map too small" seen.
+
+dir=/tmp
+odir=`pwd`
+cd $dir
+sed '1,/^EOF/d' < $odir/$0 > $dir/shm_open.c
+cc -o shm_open -Wall -Wextra -O2 -g shm_open.c || exit 1
+rm -f shm_open.c
+cd $odir
+
+su $testuser -c /tmp/shm_open
+
+rm -f /tmp/shm_open
+exit
+
+EOF
+#include <err.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+int
+main(void)
+{
+	int fd, i;
+	off_t len;
+	char name[80];
+
+	len = 1024 * 1024;
+	for (i = 0; i < 500000; i++) {
+		sprintf(name, "/testing.%06d", i);
+
+		if ((fd = shm_open(name, O_RDWR | O_CREAT, 0644)) == -1)
+			err(1, "shm_open()");
+		if (ftruncate(fd, len) == -1)
+			err(1, "ftruncate");
+		close(fd);
+	}
+
+	return(0);
+}
