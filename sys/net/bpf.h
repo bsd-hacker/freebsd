1f242ccced9ef0e98859cb1c3a014fc17ebd8f1c
syuu <syuu@FreeBSD.org>
2013-09-03 19:07:01 +0000

    bpfq.h

--- sys/net/bpf.h
+++ sys/net/bpf.h
@@ -40,6 +40,8 @@
 #ifndef _NET_BPF_H_
 #define _NET_BPF_H_
 
+#include <net/bpfq.h>
+
 /* BSD style release date */
 #define	BPF_RELEASE 199606
 
@@ -114,36 +116,6 @@ struct bpf_zbuf {
 	void	*bz_bufb;	/* Location of 'b' zero-copy buffer. */
 	size_t	 bz_buflen;	/* Size of zero-copy buffers. */
 };
-
-#ifndef _KERNEL
-#include <sys/param.h>
-#include <sys/bitset.h>
-#include <sys/_bitset.h>
-
-#define BPFQ_BITS			256
-BITSET_DEFINE(bpf_qmask_bits, BPFQ_BITS);
-
-#define	BPFQ_CLR(n, p)			BIT_CLR(BPFQ_BITS, n, p)
-#define	BPFQ_COPY(f, t)			BIT_COPY(BPFQ_BITS, f, t)
-#define	BPFQ_ISSET(n, p)		BIT_ISSET(BPFQ_BITS, n, p)
-#define	BPFQ_SET(n, p)			BIT_SET(BPFQ_BITS, n, p)
-#define	BPFQ_ZERO(p) 			BIT_ZERO(BPFQ_BITS, p)
-#define	BPFQ_FILL(p) 			BIT_FILL(BPFQ_BITS, p)
-#define	BPFQ_SETOF(n, p)		BIT_SETOF(BPFQ_BITS, n, p)
-#define	BPFQ_EMPTY(p)			BIT_EMPTY(BPFQ_BITS, p)
-#define	BPFQ_ISFULLSET(p)		BIT_ISFULLSET(BPFQ_BITS, p)
-#define	BPFQ_SUBSET(p, c)		BIT_SUBSET(BPFQ_BITS, p, c)
-#define	BPFQ_OVERLAP(p, c)		BIT_OVERLAP(BPFQ_BITS, p, c)
-#define	BPFQ_CMP(p, c)			BIT_CMP(BPFQ_BITS, p, c)
-#define	BPFQ_OR(d, s)			BIT_OR(BPFQ_BITS, d, s)
-#define	BPFQ_AND(d, s)			BIT_AND(BPFQ_BITS, d, s)
-#define	BPFQ_NAND(d, s)			BIT_NAND(BPFQ_BITS, d, s)
-#define	BPFQ_CLR_ATOMIC(n, p)		BIT_CLR_ATOMIC(BPFQ_BITS, n, p)
-#define	BPFQ_SET_ATOMIC(n, p)		BIT_SET_ATOMIC(BPFQ_BITS, n, p)
-#define	BPFQ_OR_ATOMIC(d, s)		BIT_OR_ATOMIC(BPFQ_BITS, d, s)
-#define	BPFQ_COPY_STORE_REL(f, t)	BIT_COPY_STORE_REL(BPFQ_BITS, f, t)
-#define	BPFQ_FFS(p)			BIT_FFS(BPFQ_BITS, p)
-#endif
 
 #define	BIOCGBLEN	_IOR('B', 102, u_int)
 #define	BIOCSBLEN	_IOWR('B', 102, u_int)
--- sys/net/bpfdesc.h
+++ sys/net/bpfdesc.h
@@ -43,12 +43,8 @@
 #include <sys/selinfo.h>
 #include <sys/queue.h>
 #include <sys/conf.h>
-#include <sys/bitset.h>
-#include <sys/_bitset.h>
 #include <net/if.h>
-
-#define BPFQ_BITS			256
-BITSET_DEFINE(bpf_qmask_bits, BPFQ_BITS);
+#include <net/bpfq.h>
 
 struct bpf_qmask {
 	int		qm_enabled;
@@ -64,27 +60,6 @@ struct bpf_qmask {
 #define BPFQ_RUNLOCK(qm)	rw_runlock(&(qm)->qm_lock)
 #define BPFQ_WLOCK(qm)		rw_wlock(&(qm)->qm_lock)
 #define BPFQ_WUNLOCK(qm)	rw_wunlock(&(qm)->qm_lock)
-
-#define	BPFQ_CLR(n, p)			BIT_CLR(BPFQ_BITS, n, p)
-#define	BPFQ_COPY(f, t)			BIT_COPY(BPFQ_BITS, f, t)
-#define	BPFQ_ISSET(n, p)		BIT_ISSET(BPFQ_BITS, n, p)
-#define	BPFQ_SET(n, p)			BIT_SET(BPFQ_BITS, n, p)
-#define	BPFQ_ZERO(p) 			BIT_ZERO(BPFQ_BITS, p)
-#define	BPFQ_FILL(p) 			BIT_FILL(BPFQ_BITS, p)
-#define	BPFQ_SETOF(n, p)		BIT_SETOF(BPFQ_BITS, n, p)
-#define	BPFQ_EMPTY(p)			BIT_EMPTY(BPFQ_BITS, p)
-#define	BPFQ_ISFULLSET(p)		BIT_ISFULLSET(BPFQ_BITS, p)
-#define	BPFQ_SUBSET(p, c)		BIT_SUBSET(BPFQ_BITS, p, c)
-#define	BPFQ_OVERLAP(p, c)		BIT_OVERLAP(BPFQ_BITS, p, c)
-#define	BPFQ_CMP(p, c)			BIT_CMP(BPFQ_BITS, p, c)
-#define	BPFQ_OR(d, s)			BIT_OR(BPFQ_BITS, d, s)
-#define	BPFQ_AND(d, s)			BIT_AND(BPFQ_BITS, d, s)
-#define	BPFQ_NAND(d, s)			BIT_NAND(BPFQ_BITS, d, s)
-#define	BPFQ_CLR_ATOMIC(n, p)		BIT_CLR_ATOMIC(BPFQ_BITS, n, p)
-#define	BPFQ_SET_ATOMIC(n, p)		BIT_SET_ATOMIC(BPFQ_BITS, n, p)
-#define	BPFQ_OR_ATOMIC(d, s)		BIT_OR_ATOMIC(BPFQ_BITS, d, s)
-#define	BPFQ_COPY_STORE_REL(f, t)	BIT_COPY_STORE_REL(BPFQ_BITS, f, t)
-#define	BPFQ_FFS(p)			BIT_FFS(BPFQ_BITS, p)
 
 /*
  * Descriptor associated with each open bpf file.
