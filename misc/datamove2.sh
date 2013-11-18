431d513f72ca8a44ab7229c0c15ae21df6c472b4
pho <pho@FreeBSD.org>
2013-11-18 11:29:25 +0000

    The original copyright notice was missing. Reduce run time for datamove2.sh.
    Sponsored by:	EMC / Isilon storage division

--- misc/datamove2.sh
+++ misc/datamove2.sh
@@ -40,7 +40,7 @@ sed '1,/^EOF/d' < $here/$0 > dl.c
 cc -o dl -Wall dl.c
 rm -f dl.c
 
-for i in `jot 3`; do
+for i in `jot 2`; do
 	$here/../testcases/swap/swap -t 10m -i 200 -h &
 	/tmp/dl 
 	ps | grep swap | grep -v swap | awk '{print $1}' | xargs kill
@@ -48,6 +48,32 @@ done
 rm -rf /tmp/dl
 exit 0
 EOF
+/*-
+ * Copyright (c) 2006, Stephan Uphoff <ups@freebsd.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice unmodified, this list of conditions, and the following
+ *    disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <err.h>
 #include <fcntl.h>
 #include <stdio.h>
