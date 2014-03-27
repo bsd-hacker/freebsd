a526f9c0c070b78b7a8e5ad43d6a525d3a18539d
des <des@FreeBSD.org>
2014-03-27 08:48:19 +0000

    Clean up and document

--- db/update.sh
+++ db/update.sh
@@ -3,6 +3,13 @@
 # $FreeBSD$
 #
 
+user=$1
+password=$2
+
 cd $(dirname $(realpath $0))
 
-../script/fbce_create.pl model FBCE DBIC::Schema FBCE::Schema
+../script/fbce_create.pl model FBCE DBIC::Schema FBCE::Schema \
+    create=static \
+    overwrite_modifications=1 \
+    "dbi:Pg:dbname=fbce" "${user:-fbce}" "$password" \
+    pg_enable_utf8=1
