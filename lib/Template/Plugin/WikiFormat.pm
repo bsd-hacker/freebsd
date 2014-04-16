e78a8bb6ee5384005d650439271cc2af96866e06
des <des@FreeBSD.org>
2014-04-16 21:11:01 +0000

    Template::Toolkit plugin for Text::WikiFormat.

---
+++ lib/Template/Plugin/WikiFormat.pm
@@ -0,0 +1,63 @@
+use utf8;
+package Template::Plugin::WikiFormat;
+
+=head1 NAME
+
+Template::Plugin::WikiFormat
+
+=cut
+
+use strict;
+use warnings;
+use base 'Template::Plugin::Filter';
+use Text::WikiFormat;
+
+=head1 DESCRIPTION
+
+L<Template::Toolkit> filter plugin for L<Text::WikiFormat>
+
+=head1 METHODS
+
+=head2 init
+
+The initialization function.
+
+=cut
+
+sub init($) {
+    my ($self) = @_;
+
+    my $name = $self->{_CONFIG}->{name} || 'wiki';
+    $self->{_DYNAMIC} = 1;
+    $self->install_filter($name);
+    return $self;
+}
+
+=head2 filter
+
+The filter function.
+
+=cut
+
+sub filter($$) {
+    my ($self, $raw) = @_;
+
+    print(STDERR "Template::Plugin::WikiFormat::filter()\n");
+    return Text::WikiFormat::format($raw, {}, {
+	implicit_links => 0, extended => 1, absolute_links => 1 });
+}
+
+=head1 AUTHOR
+
+Dag-Erling Smørgrav <des@freebsd.org>
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
+
+# $FreeBSD$
