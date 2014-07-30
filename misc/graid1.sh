779fd7ff8fae8665b2880843754d5a933ec79c80
pho <pho@FreeBSD.org>
2014-07-30 08:36:48 +0000

    Added a simple raid0 and raid1 test scenario.
    Sponsored by:	 EMC / Isilon storage division

---
+++ misc/graid1.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+
+#
+# Copyright (c) 2014 EMC Corp.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+# $FreeBSD$
+#
+
+# No problems seen.
+
+[ `id -u ` -ne 0 ] && echo "Must be root!" && exit 1
+
+. ../default.cfg
+
+md1=$mdstart
+md2=$((mdstart + 1))
+md3=$((mdstart + 2))
+
+for u in $md1 $md2 $md3; do
+	mdconfig -l | grep -q md$u && mdconfig -d -u $u
+	mdconfig -a -t swap -s 1g -u $u
+done
+
+gmirror load || exit 1
+gmirror label -v -b split -s 2048 data /dev/md$md1 /dev/md$md2 \
+    /dev/md$md3 > /dev/null
+newfs $newfs_flags /dev/mirror/data > /dev/null
+mount /dev/mirror/data $mntpoint
+chmod 777 $mntpoint
+
+export runRUNTIME=10m
+export RUNDIR=$mntpoint/stressX
+
+su $testuser -c 'cd ..; ./run.sh marcus.cfg'
+while mount | grep $mntpoint | grep -q /mirror/; do
+	umount $mntpoint || sleep 1
+done
+gmirror stop data
+gmirror unload
+
+for u in $md3 $md2 $md1; do
+	mdconfig -d -u $u
+done
