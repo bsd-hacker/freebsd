f15767f16a4e4e50cf033fc246994d78fc8e3546
ngie <ngie@FreeBSD.org>
2014-09-10 11:01:18 +0000

    Reduce nested directories by 1

--- contrib/pjdfstest/tests/misc.sh
+++ contrib/pjdfstest/tests/misc.sh
@@ -4,14 +4,14 @@ ntest=1
 
 case "${dir}" in
 /*)
-	maindir="${dir}/../.."
+	maindir="${dir}/.."
 	;;
 *)
-	maindir="`pwd`/${dir}/../.."
+	maindir="`pwd`/${dir}/.."
 	;;
 esac
 fstest="${maindir}/pjdfstest"
-. ${maindir}/tests/conf
+. ${maindir}/conf
 
 expect()
 {
