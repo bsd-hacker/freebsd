bsd-hacker/freebsd

commit 13e88c3ac7c5b5b8486b8a295f57a6a06aeedc01

vgapci: New vga_pci_repost() function
This can be used to restore the VGA mode after a KMS driver is unloaded.

Differential Revision:	https://reviews.freebsd.org/D687


git-svn-id: svn+ssh://svn.freebsd.org/base/head@279487 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/dev/pci/vga_pci.c
@@ -51,6 +51,8 @@ __FBSDID("$FreeBSD$");
 #include <dev/pci/pcireg.h>
 #include <dev/pci/pcivar.h>
 
+#include <compat/x86bios/x86bios.h> /* To re-POST the card. */
+
 struct vga_resource {
 	struct resource	*vr_res;
 	int	vr_refs;
@@ -192,6 +194,36 @@ vga_pci_unmap_bios(device_t dev, void *bios)
 	    ("vga_pci_unmap_bios: mismatch"));
 	vga_pci_release_resource(dev, NULL, SYS_RES_MEMORY, PCIR_BIOS,
 	    vr->vr_res);
+}
+
+int
+vga_pci_repost(device_t dev)
+{
+#if defined(__amd64__) || defined(__i386__)
+	x86regs_t regs;
+
+	if (!vga_pci_is_boot_display(dev))
+		return (EINVAL);
+
+	if (x86bios_get_orm(VGA_PCI_BIOS_SHADOW_ADDR) == NULL)
+		return (ENOTSUP);
+
+	x86bios_init_regs(&regs);
+
+	regs.R_AH = pci_get_bus(dev);
+	regs.R_AL = (pci_get_slot(dev) << 3) | (pci_get_function(dev) & 0x07);
+	regs.R_DL = 0x80;
+
+	device_printf(dev, "REPOSTing\n");
+	x86bios_call(&regs, X86BIOS_PHYSTOSEG(VGA_PCI_BIOS_SHADOW_ADDR + 3),
+	    X86BIOS_PHYSTOOFF(VGA_PCI_BIOS_SHADOW_ADDR + 3));
+
+	x86bios_get_intr(0x10);
+
+	return (0);
+#else
+	return (ENOTSUP);
+#endif
 }
 
 static int

sys/dev/pci/pcivar.h
@@ -591,5 +591,6 @@ struct pcicfg_vpd *pci_fetch_vpd_list(device_t dev);
 int	vga_pci_is_boot_display(device_t dev);
 void *	vga_pci_map_bios(device_t dev, size_t *size);
 void	vga_pci_unmap_bios(device_t dev, void *bios);
+int	vga_pci_repost(device_t dev);
 
 #endif /* _PCIVAR_H_ */

commit 05d77468674d5bc67d3aaab12b982566b603a97a

Record the dependency to x86bios in vga_pci
This fixes the build of XEN and XBOX kernels on i386, which was broken
in r279487.

While here, do not build vga_pci_repost() on PC98.

Reported by:	bz@


git-svn-id: svn+ssh://svn.freebsd.org/base/head@279494 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/conf/files.amd64
@@ -513,10 +513,10 @@ compat/ndis/winx64_wrap.S	optional	ndisapi pci
 libkern/memmove.c		standard
 libkern/memset.c		standard
 #
-# x86 real mode BIOS emulator, required by atkbdc/dpms/vesa
+# x86 real mode BIOS emulator, required by atkbdc/dpms/pci/vesa
 #
-compat/x86bios/x86bios.c	optional x86bios | atkbd | dpms | vesa
-contrib/x86emu/x86emu.c		optional x86bios | atkbd | dpms | vesa
+compat/x86bios/x86bios.c	optional x86bios | atkbd | dpms | pci | vesa
+contrib/x86emu/x86emu.c		optional x86bios | atkbd | dpms | pci | vesa
 #
 # bvm console
 #

sys/conf/files.i386
@@ -531,9 +531,9 @@ i386/xbox/xboxfb.c		optional xboxfb
 dev/fb/boot_font.c		optional xboxfb
 i386/xbox/pic16l.s		optional xbox
 #
-# x86 real mode BIOS support, required by atkbdc/dpms/vesa
+# x86 real mode BIOS support, required by atkbdc/dpms/pci/vesa
 #
-compat/x86bios/x86bios.c	optional x86bios | atkbd | dpms | vesa
+compat/x86bios/x86bios.c	optional x86bios | atkbd | dpms | pci | vesa
 #
 # bvm console
 #

sys/dev/pci/vga_pci.c
@@ -199,7 +199,7 @@ vga_pci_unmap_bios(device_t dev, void *bios)
 int
 vga_pci_repost(device_t dev)
 {
-#if defined(__amd64__) || defined(__i386__)
+#if defined(__amd64__) || (defined(__i386__) && !defined(PC98))
 	x86regs_t regs;
 
 	if (!vga_pci_is_boot_display(dev))
@@ -633,3 +633,4 @@ static driver_t vga_pci_driver = {
 static devclass_t vga_devclass;
 
 DRIVER_MODULE(vgapci, pci, vga_pci_driver, vga_devclass, 0, 0);
+MODULE_DEPEND(vgapci, x86bios, 1, 1, 1);