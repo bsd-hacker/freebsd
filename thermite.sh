6dde7b209cc87f9192962ad1065809ca0ca466ec
gjb <gjb@FreeBSD.org>
2015-05-28 15:58:41 +0000

    Re-sync with the RE repository:  In build_ec2_ami(), fix AWSKEYFILE evaluation, use  the correct src tree path for the 'ec2ami' target,  and redirect stderr to stdout.
    Sponsored by:	The FreeBSD Foundation

--- thermite.sh
+++ thermite.sh
@@ -314,15 +314,15 @@ build_ec2_ami() {
 			return 0
 		fi
 	fi
-	if [ -z "${AWSREGION}" -o -z "${AWSBUCKET}" -o "${AWSKEYFILE}" ]; then
+	if [ -z "${AWSREGION}" -o -z "${AWSBUCKET}" -o -z "${AWSKEYFILE}" ]; then
 		return 0
 	fi
-	chroot ${CHROOTDIR} make -C /usr/src \
+	chroot ${CHROOTDIR} make -C /usr/src/release \
 		AWSREGION=${AWSREGION} \
 		AWSBUCKET=${AWSBUCKET} \
 		AWSKEYFILE=${AWSKEYFILE} \
 		EC2PUBLIC=${EC2PUBLIC} ec2ami \
-		>> ${logdir}/${_build}.log
+		>> ${logdir}/${_build}.log 2>&1
 	unset _build _conf AWSREGION AWSBUCKET AWSKEYFILE EC2PUBLIC
 	return 0
 } # build_ec2_ami()
