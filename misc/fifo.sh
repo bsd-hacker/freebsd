fb2cb872bb12fa2c385b29b44ab83f71b290aec9
pho <pho@FreeBSD.org>
2016-01-17 12:13:40 +0000

    Added finding.
    Sponsored by:	EMC / Isilon storage division

--- misc/fifo.sh
+++ misc/fifo.sh
@@ -32,6 +32,8 @@
 # http://people.freebsd.org/~pho/stress/log/kostik652.txt
 # Fixed by r259522.
 
+# https://people.freebsd.org/~pho/stress/log/kostik863.txt
+
 [ `id -u ` -ne 0 ] && echo "Must be root!" && exit 1
 
 . ../default.cfg
