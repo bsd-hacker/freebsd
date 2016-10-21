33d1497b11fd40036aea4ca497606912b3984efe
markj <markj@FreeBSD.org>
2016-10-21 19:40:37 +0000

    Have madvise(MADV_DONTNEED) move dirty pages to the laundry queue.
    Previously we would always requeue such pages at the tail of the inactive
    queue, which would further delay reclamation of a page already in the
    laundry queue.
    
    Discussed with:	alc

--- sys/vm/vm_page.c
+++ sys/vm/vm_page.c
@@ -3310,11 +3310,13 @@ vm_page_advise(vm_page_t m, int advice)
 	/*
 	 * Place clean pages near the head of the inactive queue rather than
 	 * the tail, thus defeating the queue's LRU operation and ensuring that
-	 * the page will be reused quickly.  Dirty pages are given a chance to
-	 * cycle once through the inactive queue before becoming eligible for
-	 * laundering.
+	 * the page will be reused quickly.  Dirty pages not already in the
+	 * laundry are moved there.
 	 */
-	_vm_page_deactivate(m, m->dirty == 0);
+	if (m->dirty == 0)
+		_vm_page_deactivate(m, TRUE);
+	else
+		vm_page_launder(m);
 }
 
 /*
