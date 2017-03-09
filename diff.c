4385e8f8189bdcf6add6bc53aa242a453a965c1a
bapt <bapt@FreeBSD.org>
2017-03-09 12:13:56 +0000

    Update the usage() to reflect current reality

--- diff.c
+++ diff.c
@@ -435,13 +435,20 @@ void
 usage(void)
 {
 	(void)fprintf(stderr,
-	    "usage: diff [-aBbdilpTtw] [-c | -e | -f | -n | -q | -u] [-I pattern] [-L label]\n"
-	    "            file1 file2\n"
-	    "       diff [-aBbdilpTtw] [-I pattern] [-L label] -C number file1 file2\n"
-	    "       diff [-aBbdiltw] [-I pattern] -D string file1 file2\n"
-	    "       diff [-aBbdilpTtw] [-I pattern] [-L label] -U number file1 file2\n"
-	    "       diff [-aBbdilNPprsTtw] [-c | -e | -f | -n | -q | -u] [-I pattern]\n"
-	    "            [-L label] [-S name] [-X file] [-x pattern] dir1 dir2\n");
+	    "usage: diff [-aBbdilpTtw] [-c | -e | -f | -n | -q | -u] [--ignore-case]\n"
+	    "            [--no-ignore-case] [--normal] [--strip-trailing-cr] [--tabsize]\n"
+	    "            [-I pattern] [-L label] file1 file2\n"
+	    "       diff [-aBbdilpTtw] [-I pattern] [-L label] [--ignore-case]\n"
+	    "            [--no-ignore-case] [--normal] [--strip-trailing-cr] [--tabsize]\n"
+	    "            -C number file1 file2\n"
+	    "       diff [-aBbdiltw] [-I pattern] [--ignore-case] [--no-ignore-case]\n"
+	    "            [--normal] [--strip-trailing-cr] [--tabsize] -D string file1 file2\n"
+	    "       diff [-aBbdilpTtw] [-I pattern] [-L label] [--ignore-case]\n"
+	    "            [--no-ignore-case] [--normal] [--tabsize] [--strip-trailing-cr]\n"
+	    "            -U number file1 file2\n"
+	    "       diff [-aBbdilNPprsTtw] [-c | -e | -f | -n | -q | -u] [--ignore-case]\n"
+	    "            [--no-ignore-case] [--normal] [--tabsize] [-I pattern] [-L label]\n"
+	    "            [-S name] [-X file] [-x pattern] dir1 dir2\n");
 
 	exit(2);
 }
